71f44bd45337c26f7d1764f5dc41fd52d84fb0d8
Vijay Khemlani <vkhemlan@gmail.com>
2017-09-28 18:29:26 -0300

    * Finished implementing quick version of current prices report

--- reports/models.py
+++ reports/models.py
@@ -3,7 +3,7 @@ import xlsxwriter
 from django.core.files.base import ContentFile
 from django.db import models
 
-from solotodo.models import Entity
+from solotodo.models import Entity, Product
 from solotodo_try.s3utils import PrivateS3Boto3Storage
 
 
@@ -15,45 +15,112 @@ class Report(models.Model):
 
     def render_current_prices(self, user):
         es = Entity.objects.filter(product__isnull=False)\
+            .filter(category=1)\
             .get_available()\
             .filter_by_user_perms(user, 'view_entity')\
-            .select_related()
+            .select_related()\
+            .order_by('product')
+
+        product_ids = [x['product'] for x in es.values('product')]
+
+        es_search = Product.es_search().filter('terms', product_id=product_ids)
+        es_dict = {e.product_id: e.to_dict()
+                   for e in es_search[:100000].execute()}
+
+        print(product_ids)
 
         output = io.BytesIO()
 
         # Create a workbook and add a worksheet.
         workbook = xlsxwriter.Workbook(output)
+        workbook.formats[0].set_font_size(10)
         worksheet = workbook.add_worksheet()
 
+        url_format = workbook.add_format({
+            'font_color': 'blue',
+            'font_size': 10
+        })
+
+        header_format = workbook.add_format({
+            'bold': True,
+            'font_size': 10
+        })
+
+        es_fields = [
+            ('Marca', 'line_brand_unicode'),
+            ('Modelo', 'model_name'),
+            (u'Línea Procesador', 'processor_line_unicode'),
+            ('Procesador', 'processor_unicode'),
+            ('RAM', 'ram_quantity_unicode'),
+            ('Pantalla', 'screen_size_unicode'),
+            (u'Resolución pantalla', 'screen_resolution_unicode'),
+            ('Tarjeta de video integrada', 'processor_gpu_unicode'),
+            ('Tarjeta de video dedicada', 'dedicated_video_card_unicode'),
+            (u'Unidad óptica', 'optical_drive_unicode'),
+            (u'Pantalla táctil', 'screen_is_touchscreen'),
+            (u'Sistema operativo', 'operating_system_unicode'),
+            (u'Sistema operativo (corto)', 'operating_system_short_name'),
+            ('Tipo almacenamiento',
+             'largest_storage_drive_drive_type_unicode'),
+            ('Capacidad almacenamiento',
+             'largest_storage_drive_capacity_unicode'),
+            (u'Batería', 'pretty_battery'),
+            ('Peso (g)', 'weight'),
+            (u'Tamaño', 'pretty_dimensions'),
+            ('Puertos de video', 'pretty_video_ports'),
+            ('Bluetooth', 'has_bluetooth'),
+            ('WiFi', 'wifi_card_unicode'),
+            ('Puertos USB', 'usb_port_count'),
+            ('Webcam (MP)', 'webcam_mp'),
+            ('LAN', 'lan_unicode'),
+            (u'Adaptador de energía', 'power_adapter_unicode'),
+            ('Lector de tarjetas', 'card_reader_unicode'),
+            (u'¿Lector de huellas?', 'has_fingerprint_reader')
+        ]
+
         headers = [
-            'Nombre',
-            'Tienda',
+            'Producto',
             'Categoría',
+            'Tienda',
             'SKU',
             'Precio normal',
             'Precio oferta',
-            'Moneda',
-            'URL',
-            'Producto',
-            'Fecha detección'
+            'Nombre'
         ]
 
-        for idx, header in enumerate(headers):
-            worksheet.write(0, idx, header)
+        headers.extend([e[0] for e in es_fields])
 
-        for idx, e in enumerate(es):
-            row = idx + 1
-            worksheet.write(row, 0, e.name)
-            worksheet.write(row, 1, str(e.store))
-            worksheet.write(row, 2, str(e.category))
-            worksheet.write(row, 3, e.sku)
+        for idx, header in enumerate(headers):
+            worksheet.write(0, idx, header, header_format)
+
+        row = 1
+        for e in es:
+            es_entry = es_dict[e.product_id]
+            print(e.id)
+
+            worksheet.write_url(
+                row, 0,
+                'http://local.solotodo.com:3000/products/' + str(e.product.id),
+                string=str(e.product),
+                cell_format=url_format)
+            worksheet.write(row, 1, str(e.category))
+            worksheet.write(row, 2, str(e.store))
+            worksheet.write_url(
+                row, 3,
+                'http://local.solotodo.com:3000/entities/' + str(e.id),
+                string=e.sku,
+                cell_format=url_format)
             worksheet.write(row, 4, e.active_registry.normal_price)
             worksheet.write(row, 5, e.active_registry.offer_price)
-            worksheet.write(row, 6, e.currency.iso_code)
-            worksheet.write(row, 7, e.url)
-            worksheet.write(row, 8, str(e.product))
-            worksheet.write(row, 9, e.creation_date.date().strftime(
-                '%Y-%m-%d'))
+            worksheet.write(row, 6, e.name)
+
+            col = 7
+            for idx, field in enumerate(es_fields):
+                worksheet.write(row, col + idx, es_entry.get(field[1], 'N/A'))
+
+            row += 1
+
+        worksheet.autofilter(0, 0, row-1, len(headers)-1)
 
         workbook.close()
 
--- reports/views.py
+++ reports/views.py
@@ -1,5 +1,7 @@
 from guardian.shortcuts import get_objects_for_user
 from rest_framework import viewsets
+from rest_framework.decorators import list_route
+from rest_framework.response import Response
 
 from reports.models import Report
 from reports.serializers import ReportSerializer
@@ -11,3 +13,11 @@ class ReportViewSet(viewsets.ReadOnlyModelViewSet):
 
     def get_queryset(self):
         return get_objects_for_user(self.request.user, 'view_report', Report)
+
+    @list_route()
+    def current_prices(self, request):
+        report = Report.objects.get(pk=1)
+        report_url = report.render_current_prices(request.user)
+        return Response({
+            'url': report_url
+        })

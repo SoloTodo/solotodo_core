27549faa2a7ef393e48ab613fbb2e50d9944249b
Vijay Khemlani <vkhemlan@gmail.com>
2018-01-11 13:08:51 -0300

    * Added mathfilters

--- solotodo_core/settings/defaults.py
+++ solotodo_core/settings/defaults.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'mathfilters',
     'guardian',
     'rest_framework',
     'rest_framework.authtoken',

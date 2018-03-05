f6c199f44b47185e511a5cad482dd04e3f039a5a
Vijay Khemlani <vkhemlan@gmail.com>
2018-03-05 15:03:45 -0300

    * Added new resource options

--- solotodo/forms/resource_names_form.py
+++ solotodo/forms/resource_names_form.py
@@ -1,8 +1,12 @@
 from django import forms
 
-from solotodo.models import Country, Currency, StoreType, Store, NumberFormat
+from category_templates.models import CategoryTemplate
+from category_templates.serializers import CategoryTemplateSerializer
+from solotodo.models import Country, Currency, StoreType, Store, \
+    NumberFormat, Category
 from solotodo.serializers import CountrySerializer, CurrencySerializer, \
-    StoreTypeSerializer, StoreSerializer, NumberFormatSerializer
+    StoreTypeSerializer, StoreSerializer, NumberFormatSerializer, \
+    CategorySerializer
 
 
 class ResourceNamesForm(forms.Form):
@@ -30,6 +34,16 @@ class ResourceNamesForm(forms.Form):
         'number_formats': {
             'model': NumberFormat,
             'serializer': NumberFormatSerializer,
+            'permission': None
+        },
+        'categories': {
+            'model': Category,
+            'serializer': CategorySerializer,
+            'permission': 'view_category'
+        },
+        'category_templates': {
+            'model': CategoryTemplate,
+            'serializer': CategoryTemplateSerializer,
             'permission': None
         }
     }

7ee1f5d862ae81293035ddc56ea36883870cb2d4
Vijay Khemlani <vkhemlan@gmail.com>
2018-03-07 08:21:59 -0300

    * Updated default cache

--- solotodo_core/settings/defaults.py
+++ solotodo_core/settings/defaults.py
@@ -190,8 +190,7 @@ GEOIP_PATH = BASE_DIR
 
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '127.0.0.1:11211',
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
     }
 }
 

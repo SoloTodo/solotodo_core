ea53aeb8ae2365c5c50e3e886f4728d7b7747a4b
Vijay Khemlani <vkhemlan@gmail.com>
2018-03-16 11:30:51 -0300

    * Added optional stores parameter to store_update_pricings management command

--- solotodo/management/commands/store_update_pricings.py
+++ solotodo/management/commands/store_update_pricings.py
@@ -5,8 +5,15 @@ from solotodo.tasks import store_update
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('--stores', nargs='*', type=str)
+
     def handle(self, *args, **options):
+        store_names = options['stores']
         stores = Store.objects.filter(is_active=True)
+
+        if store_names:
+            stores = stores.filter(name__in=store_names)
 
         for store in stores:
             try:

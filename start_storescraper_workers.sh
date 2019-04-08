89cd24efa5a6fc8aa0ac35e160edaf4a3eccd403
Vijay Khemlani <vkhemlan@gmail.com>
2019-04-08 10:43:54 -0400

    * Fixed dependencies, celery>=4.2 does not work well

--- start_storescraper_workers.sh
+++ start_storescraper_workers.sh
@@ -3,4 +3,4 @@ source ~/.zshrc
 
 cd "${0%/*}"
 source env/bin/activate
-celery multi start -A solotodo_core storescraper -Q:storescraper storescraper -c:storescraper 30 --logfile=solotodo_core/logs/celery/%n.log --pidfile=solotodo_core/pids/celery/%n.pid -E -l info
+celery multi start -A solotodo_core storescraper -Q:storescraper storescraper -c:storescraper 50 --logfile=solotodo_core/logs/celery/%n.log --pidfile=solotodo_core/pids/celery/%n.pid -E -l info

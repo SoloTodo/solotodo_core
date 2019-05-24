b682b84e42d304fab0ab136a39c2253ec842abf9
david <david@solotodo.com>
2019-05-24 13:59:36 -0400

    ADDED: task for current prices report

--- reports/tasks.py
+++ reports/tasks.py
@@ -3,8 +3,49 @@ from django.core.mail import EmailMessage
 from django.http import QueryDict
 
 from reports.forms.report_daily_prices_form import ReportDailyPricesForm
+from reports.forms.report_current_prices_form import ReportCurrentPricesForm
 from reports.models import Report, ReportDownload
 from solotodo.models import SoloTodoUser
+
+
+@shared_task(queue='general', ignore_result=True, task_time_limit=1800)
+def send_current_prices_task(user_id, query_string):
+    report = Report.objects.get(slug='current_prices')
+    user = SoloTodoUser.objects.get(id=user_id)
+
+    q_dict = QueryDict(query_string)
+
+    form = ReportCurrentPricesForm(user, q_dict)
+    assert form.is_valid()
+
+    category = form.cleaned_data['category']
+    spec_form_class = category.specs_form()
+    spec_form = spec_form_class(q_dict)
+
+    assert spec_form.is_valid()
+
+    es_products_search = spec_form.get_es_products()
+    report_data = form.generate_report(es_products_search)
+
+    report_filename = '{}.xlsx'.format(report_data['filename'])
+    report_file = report_data['file']
+    report_path = report_data['path']
+
+    ReportDownload.objects.create(
+        report=report,
+        user=user,
+        file=report_path)
+
+    sender = SoloTodoUser().get_bot().email_recipient_text()
+    message = 'Se adjunta el reporte de precios actuales para la categoría  ' \
+              '"{}"'.format(form.cleaned_data['category'])
+
+    email = EmailMessage('Reporte precios actuales', message, sender,
+                         [user.email])
+    email.attach(
+        report_filename, report_file,
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    email.send()
 
 
 @shared_task(queue='general', ignore_result=True, task_time_limit=1800)

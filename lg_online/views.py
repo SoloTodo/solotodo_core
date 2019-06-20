0c0f15b53503deb652ace9f314a054ca8a0a2887
Vijay Khemlani <vkhemlan@gmail.com>
2019-06-20 16:02:08 -0400

    * Added JumboStore to LG Online feed

--- lg_online/views.py
+++ lg_online/views.py
@@ -114,7 +114,7 @@ class LgOnlineFeedViewSet(ViewSet):
         products_metadata = {x['productId']: x for x in json_entries}
 
         store_ids = [9, 18, 11, 5, 30, 60, 67, 37, 38, 61, 12, 85, 43, 23,
-                     97, 87, 167, 86, 181, 195, 197]
+                     97, 87, 167, 86, 181, 195, 197, 224]
 
         products = Product.objects.filter(
             pk__in=list(products_metadata.keys()))

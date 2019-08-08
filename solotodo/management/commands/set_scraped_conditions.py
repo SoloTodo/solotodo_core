810646b2df32bdc230eda08d87473e3986216563
Vijay Khemlani <vkhemlan@gmail.com>
2019-08-08 15:45:38 -0400

    * Optimized set_scrper_conditions management command

--- solotodo/management/commands/set_scraped_conditions.py
+++ solotodo/management/commands/set_scraped_conditions.py
@@ -1,15 +1,14 @@
 from django.core.management import BaseCommand
+from django.db.models import F
 
 from solotodo.models import Entity
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        entities = Entity.objects.all()
+        entities = Entity.objects.exclude(scraped_condition=F('condition'))
 
         for entity in entities:
-            if entity.condition != entity.scraped_condition:
-                correct_condition = entity.condition
-                entity.scraped_condition = correct_condition
-                entity.save()
-                print(entity.id)
+            entity.scraped_condition = entity.condition
+            entity.save()
+            print(entity.id)

994b861db114f4526238caff1854ea5a72930a02
Vijay Khemlani <vkhemlan@gmail.com>
2019-10-08 12:44:53 -0300

    * Added offer price to PC Factory API endpoint

--- reports/forms/pc_factory_sku_analysis_form.py
+++ reports/forms/pc_factory_sku_analysis_form.py
@@ -43,35 +43,58 @@ class PcFactorySkuAnalysisForm:
         result = []
 
         for pcf_entity in pcf_entities:
-            pcf_price = pcf_entity.active_registry.normal_price
+            pcf_normal_price = pcf_entity.active_registry.normal_price
+            pcf_offer_price = pcf_entity.active_registry.offer_price
 
             product_entities = product_entities_dict[pcf_entity.product_id]
 
+            product_entities_sorted_by_offer = sorted(
+                product_entities,
+                key=lambda x: x.active_registry.offer_price
+            )
+
             min_normal_price = product_entities[0].active_registry.normal_price
+            min_offer_price = product_entities_sorted_by_offer[0]\
+                .active_registry.offer_price
+
             median_normal_price = product_entities[
                 len(product_entities) // 2].active_registry.normal_price
 
+            median_offer_price = product_entities_sorted_by_offer[
+                len(product_entities_sorted_by_offer) // 2]\
+                .active_registry.offer_price
+
             normal_price_delta = \
-                Decimal('100') * (pcf_price - min_normal_price) / \
+                Decimal('100') * (pcf_normal_price - min_normal_price) / \
                 min_normal_price
+
+            offer_price_delta = \
+                Decimal('100') * (pcf_offer_price - min_offer_price) / \
+                min_offer_price
 
             entry = {
                 'sku': pcf_entity.sku,
                 'product': str(pcf_entity.product),
                 'category': str(pcf_entity.product.category),
-                'pcf_normal_price': pcf_price,
+                'pcf_normal_price': pcf_normal_price,
+                'pcf_offer_price': pcf_offer_price,
                 'min_normal_price': min_normal_price,
+                'min_offer_price': min_offer_price,
                 'normal_price_delta': normal_price_delta,
+                'offer_price_delta': offer_price_delta,
                 'median_normal_price': median_normal_price,
+                'median_offer_price': median_offer_price,
                 'store_prices': []
             }
 
             for entity in product_entities:
                 entry['store_prices'].append({
-                    'store': str(entity.store),
+                    'store_id': entity.store_id,
+                    'store_name': str(entity.store),
                     'sku': str(entity.sku),
                     'url': str(entity.url),
-                    'normal_price': str(entity.active_registry.normal_price)
+                    'normal_price': int(entity.active_registry.normal_price),
+                    'offer_price': int(entity.active_registry.offer_price)
                 })
 
             result.append(entry)

9b9c1b632444c9a2b5ac7e0961b3bc0ad6700d45
David <david@fml.cl>
2020-02-17 14:08:55 -0300

    ADDED extra feedback

--- reports/tasks.py
+++ reports/tasks.py
@@ -11,48 +11,52 @@ from solotodo.models import SoloTodoUser, EsProduct
 
 @shared_task(queue='reports', ignore_result=True, task_time_limit=1800)
 def send_current_prices_task(user_id, query_string):
-    report = Report.objects.get(slug='current_prices')
-    user = SoloTodoUser.objects.get(id=user_id)
+    try:
+        report = Report.objects.get(slug='current_prices')
+        user = SoloTodoUser.objects.get(id=user_id)
 
-    q_dict = QueryDict(query_string)
+        q_dict = QueryDict(query_string)
 
-    form = ReportCurrentPricesForm(user, q_dict)
-    assert form.is_valid()
+        form = ReportCurrentPricesForm(user, q_dict)
+        assert form.is_valid()
 
-    category = form.cleaned_data['category']
-    spec_form_class = category.specs_form(form_type='es')
-    spec_form = spec_form_class(q_dict)
+        category = form.cleaned_data['category']
+        spec_form_class = category.specs_form(form_type='es')
+        spec_form = spec_form_class(q_dict)
 
-    assert spec_form.is_valid()
+        assert spec_form.is_valid()
 
-    es_products_search = EsProduct.category_search(category)
-    es_products_search = spec_form.get_es_products(es_products_search)
-    report_data = form.generate_report(es_products_search)
+        es_products_search = EsProduct.category_search(category)
+        es_products_search = spec_form.get_es_products(es_products_search)
+        report_data = form.generate_report(es_products_search)
 
-    report_filename = '{}.xlsx'.format(report_data['filename'])
-    report_file = report_data['file']
-    report_path = report_data['path']
+        report_filename = '{}.xlsx'.format(report_data['filename'])
+        report_file = report_data['file']
+        report_path = report_data['path']
 
-    ReportDownload.objects.create(
-        report=report,
-        user=user,
-        file=report_path)
+        ReportDownload.objects.create(
+            report=report,
+            user=user,
+            file=report_path)
 
-    sender = SoloTodoUser().get_bot().email_recipient_text()
-    message = 'Se adjunta el reporte de precios actuales para la categoría  ' \
-              '"{}"'.format(form.cleaned_data['category'])
+        sender = SoloTodoUser().get_bot().email_recipient_text()
+        message = 'Se adjunta el reporte de precios actuales para la categoría  ' \
+                  '"{}"'.format(form.cleaned_data['category'])
 
-    subject = 'Reporte precios actuales {} - %Y-%m-%d'.format(category)
-    subject = timezone.now().strftime(subject)
+        subject = 'Reporte precios actuales {} - %Y-%m-%d'.format(category)
+        subject = timezone.now().strftime(subject)
 
-    email = EmailMessage(subject,
-                         message, sender,
-                         [user.email])
-    email.attach(
-        report_filename, report_file,
-        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        email = EmailMessage(subject,
+                             message, sender,
+                             [user.email])
+        email.attach(
+            report_filename, report_file,
+            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
 
-    email.send()
+        email.send()
+        print('Exito')
+    except Exception as e:
+        print(e)
 
 
 @shared_task(queue='reports', ignore_result=True, task_time_limit=1800)

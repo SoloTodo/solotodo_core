3309e964d49d359a91dce0a27194c3884d239c21
Vijay Khemlani <vkhemlan@gmail.com>
2020-03-04 15:10:27 -0300

    * Fixed S20 API function

--- solotodo/views.py
+++ solotodo/views.py
@@ -1350,7 +1350,7 @@ class ProductViewSet(viewsets.ReadOnlyModelViewSet):
     def s20_bucket(self, request, *args, **kwargs):
         # Please remove this method after S20 release window
         products = Product.objects.filter(
-            instance_model__unicode_representation__contains='(G97'
+            instance_model__unicode_representation__contains='(G98'
         ).select_related('instance_model__model__category')
 
         serializer = ProductSerializer(

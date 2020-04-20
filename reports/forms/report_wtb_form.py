6254e42edac9edff68288300eeed4f85fa2cea4e
Vijay Khemlani <vkhemlan@gmail.com>
2020-04-20 12:47:11 -0400

    * Added special Samsung ruls to WTB report

--- reports/forms/report_wtb_form.py
+++ reports/forms/report_wtb_form.py
@@ -12,7 +12,7 @@ from guardian.shortcuts import get_objects_for_user
 from category_columns.models import CategoryColumn
 from wtb.models import WtbBrand, WtbEntity
 from solotodo.models import Category, Store, Entity, Country, StoreType, \
-    Currency, EsProduct
+    Currency, EsProduct, Product
 from solotodo_core.s3utils import PrivateS3Boto3Storage
 
 
@@ -105,6 +105,43 @@ class ReportWtbForm(forms.Form):
         es_dict = {e.product_id: e.to_dict()
                    for e in es_search.scan()}
 
+        # Start correction dict for Galaxy / Notes bundles
+        # Feel free to delete this part if Samsung no longer uses it
+
+        # Dictionary that maps a base model, storage and color of a cell to
+        # the part numbers of its variation without bundle
+        # ("Galaxy S20 Ultra", "128 GB", "Cosmic Black") => ["SM-G988BZK5CHO"]
+        # Used to set this part number to the report entries taht come with
+        # a bundle but share the same base model / storage / color
+        samsung_correction_dict = {}
+
+        cell_products = Product.objects.filter(
+            pk__in=product_ids, brand__name='Samsung').filter_by_category(6)
+        for cell in cell_products:
+            wtb_es = product_to_wtb_entity_dict.get(cell.id)
+            if not wtb_es:
+                continue
+
+            specs = es_dict[cell.id]['specs']
+            if specs['bundle_unicode'] != 'Sin bundle':
+                continue
+
+            key = (specs['base_model_unicode'],
+                   specs['internal_storage_unicode'],
+                   specs['color_unicode'])
+            if key in samsung_correction_dict:
+                print(wtb_es)
+                print(samsung_correction_dict[key])
+                raise Exception('Duplicate key: ' + str(key))
+            samsung_correction_dict[key] = wtb_es
+
+        # wtb entities corresponding to the S20 variants with a specific
+        # bundle, which need to be replaced with the part numbers of the
+        # variant without bundle (SM-...)
+        wtb_entity_blacklist = WtbEntity.objects.filter(key__startswith='F-')
+
+        # End correction dict for Galaxy / Notes bundle correction
+
         output = io.BytesIO()
 
         # Create a workbook and add a worksheet
@@ -193,13 +230,25 @@ class ReportWtbForm(forms.Form):
         row = 1
 
         for e in es:
-            es_entry = es_dict[e.product_id]
+            specs = es_dict[e.product_id]['specs']
 
             # Wtb
-            product_wtb_entities = product_to_wtb_entity_dict.get(
+            wtb_entities = product_to_wtb_entity_dict.get(
                 e.product_id, [None])
 
-            for wtb_entity in product_wtb_entities:
+            # Manipulate the wtb entities for the case of galaxy / note phones
+            # that was discussed above
+            for wtb_entity in wtb_entities:
+                if wtb_entity is None or wtb_entity in wtb_entity_blacklist:
+                    key = (specs['base_model_unicode'],
+                           specs['internal_storage_unicode'],
+                           specs['color_unicode'])
+                    wtb_entities = samsung_correction_dict.get(key, [None])
+                    break
+
+            # End wtb entities manipulation
+
+            for wtb_entity in wtb_entities:
                 col = 0
                 if wtb_entity:
                     worksheet.write(row, col, wtb_entity.key)
@@ -334,7 +383,7 @@ class ReportWtbForm(forms.Form):
 
                 if specs_columns:
                     for column in specs_columns:
-                        worksheet.write(row, col, es_entry['specs'].get(
+                        worksheet.write(row, col, specs.get(
                             column.field.es_field, 'N/A'))
                         col += 1
                 else:

00eb673e6ad72ba3805589cfd709f271a9f0bcc2
Vijay Khemlani <vkhemlan@gmail.com>
2020-04-27 10:14:44 -0400

    * Added flexible sitemap generator

--- solotodo/management/commands/create_solotodo_sitemaps.py
+++ solotodo/management/commands/create_solotodo_sitemaps.py
@@ -10,7 +10,18 @@ from solotodo.models import Product, Category
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('country_code', type=str, nargs=1)
+
     def handle(self, *args, **options):
+        domains_dict = {
+            'cl': 'https://www.solotodo.cl',
+            'mx': 'https://www.solotodo.com.mx',
+        }
+
+        country_code = options['country_code'].lower()
+        domain = domains_dict[country_code]
+
         group = Group.objects.get(name='base')
         categories = get_objects_for_group(group, 'view_category', Category)
 
@@ -32,14 +43,15 @@ class Command(BaseCommand):
             for product in products:
                 url = ET.SubElement(urlset, 'url')
                 loc = ET.SubElement(url, 'loc')
-                loc.text = 'https://www.solotodo.com.mx/products/{}-{}'.format(
-                    product.id, product.slug
+                loc.text = '{}/products/{}-{}'.format(
+                    domain, product.id, product.slug
                 )
                 lastmod = ET.SubElement(url, 'lastmod')
                 lastmod.text = product.last_updated.isoformat()
 
             et = ET.ElementTree(urlset)
-            file = open('sitemap_products_{}_mx.xml'.format(page + 1), 'wb')
+            file = open('sitemap_products_{}_{}.xml'.format(
+                page + 1, country_code), 'wb')
             et.write(file, encoding='utf-8', xml_declaration=True)
             file.close()
 
@@ -53,35 +65,35 @@ class Command(BaseCommand):
         for category in categories:
             url = ET.SubElement(urlset, 'url')
             loc = ET.SubElement(url, 'loc')
-            loc.text = 'https://www.solotodo.com.mx/{}'.format(category.slug)
+            loc.text = '{}/{}'.format(domain, category.slug)
 
         # Notebook processors
 
         url = ET.SubElement(urlset, 'url')
         loc = ET.SubElement(url, 'loc')
-        loc.text = 'https://www.solotodo.com.mx/notebook_processors'
+        loc.text = '{}/notebook_processors'.format(domain)
 
         model = MetaModel.objects.get(name='NotebookProcessor')
 
         for notebook_processor in model.instancemodel_set.all():
             url = ET.SubElement(urlset, 'url')
             loc = ET.SubElement(url, 'loc')
-            loc.text = 'https://www.solotodo.com.mx/notebook_processors?id={}' \
-                       ''.format(notebook_processor.id)
+            loc.text = '{}/notebook_processors?id={}'.format(
+                domain, notebook_processor.id)
 
         # Notebook video cards
 
         url = ET.SubElement(urlset, 'url')
         loc = ET.SubElement(url, 'loc')
-        loc.text = 'https://www.solotodo.com.mx/notebook_video_cards'
+        loc.text = '{}/notebook_video_cards'.format(domain)
 
         model = MetaModel.objects.get(name='NotebookVideoCard')
 
         for notebook_video_card in model.instancemodel_set.all():
             url = ET.SubElement(urlset, 'url')
             loc = ET.SubElement(url, 'loc')
-            loc.text = 'https://www.solotodo.com.mx/notebook_video_cards?id={}' \
-                       ''.format(notebook_video_card.id)
+            loc.text = '{}/notebook_video_cards?id={}'.format(
+                domain, notebook_video_card.id)
 
         # Video card GPUs
 
@@ -90,11 +102,11 @@ class Command(BaseCommand):
         for video_card_gpu in model.instancemodel_set.all():
             url = ET.SubElement(urlset, 'url')
             loc = ET.SubElement(url, 'loc')
-            loc.text = 'https://www.solotodo.com.mx/video_card_gpus/{}'.format(
-                video_card_gpu.id)
+            loc.text = '{}/video_card_gpus/{}'.format(
+                domain, video_card_gpu.id)
 
         et = ET.ElementTree(urlset)
-        file = open('sitemap_others_mx.xml', 'wb')
+        file = open('sitemap_others_{}.xml'.format(country_code), 'wb')
         et.write(file, encoding='utf-8', xml_declaration=True)
         file.close()
 
@@ -107,8 +119,9 @@ class Command(BaseCommand):
         for local_page in range(page):
             sitemap = ET.SubElement(sitemapindex, 'sitemap')
             loc = ET.SubElement(sitemap, 'loc')
-            loc.text = 'https://solotodo-core.s3.amazonaws.com/sitemaps/sitemap_products_{}_mx.xml' \
-                       ''.format(local_page + 1)
+            loc.text = 'https://solotodo-core.s3.amazonaws.com/sitemaps/' \
+                       'sitemap_products_{}_{}.xml'.format(
+                local_page + 1, country_code)
             lastmod = ET.SubElement(sitemap, 'lastmod')
             lastmod.text = timezone.now().isoformat()
 
@@ -116,9 +129,10 @@ class Command(BaseCommand):
         loc = ET.SubElement(sitemap, 'loc')
         lastmod = ET.SubElement(sitemap, 'lastmod')
         lastmod.text = timezone.now().isoformat()
-        loc.text = 'https://solotodo-core.s3.amazonaws.com/sitemaps/sitemap_others_mx.xml'
+        loc.text = 'https://solotodo-core.s3.amazonaws.com/sitemaps/' \
+                   'sitemap_others_{}.xml'.format(country_code)
 
         et = ET.ElementTree(sitemapindex)
-        file = open('sitemap_mx.xml', 'wb')
+        file = open('sitemap_{}.xml'.format(country_code), 'wb')
         et.write(file, encoding='utf-8', xml_declaration=True)
         file.close()

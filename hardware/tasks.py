f08c49073c5e89d62fa441079bef052a33a44225
Vijay Khemlani <vkhemlan@gmail.com>
2020-06-10 16:23:41 -0400

    * Removed VideoCardGpu post save tdmark score loaders * Removed legacy futuremark scores laoders

--- hardware/tasks.py
+++ hardware/tasks.py
@@ -1,12 +1,6 @@
-import logging
 from django.conf import settings
 from celery import shared_task
 
-from solotodo.models import Product
-from solotodo.tasks import product_save
-from .futuremark_utils import \
-    get_tdmark_11_score, get_tdmark_cloud_gate_score, \
-    get_tdmark_fire_strike_score
 from metamodel.models import InstanceModel
 
 
@@ -22,20 +16,3 @@ def video_card_gpu_save(instance_model_id, update_scores=True):
              # doc_type='GPU',
              id=instance_model.id,
              body=document)
-
-    # Update the GPU score in various benchmarks
-    tdmark_id = instance_model.tdmark_id
-
-    if update_scores:
-        instance_model.tdmark_11_score = get_tdmark_11_score(tdmark_id)
-        instance_model.tdmark_cloud_gate_score = \
-            get_tdmark_cloud_gate_score(tdmark_id)
-        instance_model.tdmark_fire_strike_score = \
-            get_tdmark_fire_strike_score(tdmark_id)
-
-        # Update the associated VideoCard instances so that their ElasticSearch
-        # entries are updated with the new score
-        for im in instance_model.fields_usage.all():
-            logging.info(u'saving {}'.format(im.parent))
-            product = Product.objects.get(instance_model=im.parent)
-            product_save.delay(product.id)

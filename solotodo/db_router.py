54931ad637ad49cd77376c85092ba9a5d89e0152
Vijay Khemlani <vkhemlan@gmail.com>
2020-11-06 15:25:17 -0300

    * Fixed metamodel cloning bug

--- solotodo/db_router.py
+++ solotodo/db_router.py
@@ -1,5 +1,7 @@
 class RdsDbRouter:
     def db_for_read(self, model, **hints):
+        if model._meta.app_label == 'metamodel':
+            return 'writer'
         if model._meta.app_label != 'lg_pricing':
             return 'reader'
         else:

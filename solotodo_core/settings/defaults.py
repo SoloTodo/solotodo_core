9d1d8bb621e0b23e90c7b8a81c2245f7eae7f99d
Vijay Khemlani <vkhemlan@gmail.com>
2020-11-12 12:14:43 -0300

    * Added JWT support

--- solotodo_core/settings/defaults.py
+++ solotodo_core/settings/defaults.py
@@ -276,6 +276,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',

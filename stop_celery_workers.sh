935249dafaa320c9899cbe7b730fe129fdfbd5ca
Vijay Khemlani <vkhemlan@gmail.com>
2020-11-26 13:25:19 -0300

    * Added new celery management commands

---
+++ stop_celery_workers.sh
@@ -0,0 +1,1 @@
+env/bin/celery -A solotodo_core multi stopwait store_update general reports storescraper --pidfile=solotodo_core/pids/celery/%n.pid --logfile=solotodo_core/logs/celery/%n.log -l info

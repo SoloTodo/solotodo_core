44b94c2c1404ae711e85f75d1ee5f470a4854b79
sebastian donoso <sdonoso@solotodo.com>
2020-12-02 17:30:48 -0300

    add fiel is_primitive to serializer

--- metamodel/serializers.py
+++ metamodel/serializers.py
@@ -4,18 +4,20 @@ from metamodel.models import MetaModel, MetaField, InstanceModel
 
 
 class MetaModelWithoutFieldsSerializer(serializers.HyperlinkedModelSerializer):
+    is_primitive = serializers.BooleanField('is_primitive')
     class Meta:
         model = MetaModel
-        fields = ['url', 'id', 'name', 'unicode_template', 'ordering_field']
+        fields = ['url','is_primitive', 'id', 'name', 'unicode_template', 'ordering_field']
 
 
 class MetaFieldSerializer(serializers.HyperlinkedModelSerializer):
     model = MetaModelWithoutFieldsSerializer()
+    # is_primitive = serializers.BooleanField('is_primitive')
 
     class Meta:
         model = MetaField
-        fields = ['id','name', 'ordering', 'nullable', 'multiple', 'hidden',
-                  'model']
+        fields = ['id',  'name', 'ordering', 'nullable',
+                  'multiple', 'hidden','model']
 
 
 class MetaModelSerializer(serializers.HyperlinkedModelSerializer):

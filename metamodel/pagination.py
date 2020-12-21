4c1e9f349b5d82e25de145054c3d206b44a9fba4
sebastian donoso <sdonoso@solotodo.com>
2020-12-21 16:59:42 -0300

    remove default page size

--- metamodel/pagination.py
+++ metamodel/pagination.py
@@ -2,6 +2,5 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class InstancePagination(PageNumberPagination):
-    page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 200

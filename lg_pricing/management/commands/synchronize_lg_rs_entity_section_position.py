2b9bb9430fc86ed1e48c9d94b44985b2fee3ebc3
Vijay Khemlani <vkhemlan@gmail.com>
2021-01-07 16:56:09 -0300

    * Added debugging message to RS sync command

--- lg_pricing/management/commands/synchronize_lg_rs_entity_section_position.py
+++ lg_pricing/management/commands/synchronize_lg_rs_entity_section_position.py
@@ -1,3 +1,4 @@
+from django.core.mail import send_mail
 from django.core.management import BaseCommand
 
 from lg_pricing.models import LgRsEntitySectionPosition
@@ -5,4 +6,8 @@ from lg_pricing.models import LgRsEntitySectionPosition
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        send_mail('Starting position sync', 'Started', 'solobot@solotodo.com',
+                  ['vj@soplotodo.com'])
         LgRsEntitySectionPosition.synchronize_with_db_positions()
+        send_mail('Finished position sync', 'Started', 'solobot@solotodo.com',
+                  ['vj@soplotodo.com'])

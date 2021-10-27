8e6d6cdd472dfc96dafe5a793b5190f3e249c30a
Vijay Khemlani <vkhemlan@gmail.com>
2021-10-27 16:23:04 -0300

    * Changed metamodel api endpoint

--- metamodel/routers.py
+++ metamodel/routers.py
@@ -4,7 +4,7 @@ from metamodel.views import MetaModelViewSet, InstanceModelViewSet, \
     MetaFieldViewSet, InstanceFieldViewSet
 
 router = routers.SimpleRouter()
-router.register(r'metamodel/meta_models', MetaModelViewSet)
-router.register(r'metamodel/instance_models', InstanceModelViewSet)
-router.register(r'metamodel/meta_fields', MetaFieldViewSet)
-router.register(r'metamodel/instance_fields', InstanceFieldViewSet)
+router.register(r'metamodels/meta_models', MetaModelViewSet)
+router.register(r'metamodels/instance_models', InstanceModelViewSet)
+router.register(r'metamodels/meta_fields', MetaFieldViewSet)
+router.register(r'metamodels/instance_fields', InstanceFieldViewSet)

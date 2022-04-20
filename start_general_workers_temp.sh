59227786d1bef8232e73f77a028c1629ffe355e7
Vijay Khemlani <vj@solotodo.com>
2022-04-20 16:57:43 -0400

    * Updated temp general worker concurrency

--- start_general_workers_temp.sh
+++ start_general_workers_temp.sh
@@ -3,4 +3,4 @@ source ~/.zshrc
 
 cd "${0%/*}"
 source env/bin/activate
-celery multi start -A solotodo_core general -Q:general general -c:general 50 --logfile=solotodo_core/logs/celery/%n.log --pidfile=solotodo_core/pids/celery/%n.pid -E -l info
+celery multi start -A solotodo_core general -Q:general general -c:general 8 --logfile=solotodo_core/logs/celery/%n.log --pidfile=solotodo_core/pids/celery/%n.pid -E -l info

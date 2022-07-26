adbdbdc26790ee7580bfcdee5ad161b27ea6e157
Vijay Khemlani <vj@solotodo.com>
2022-07-26 13:01:09 -0400

    * Modified send_current_prices_report management command to handle multiple categories

--- reports/management/commands/send_current_prices_report.py
+++ reports/management/commands/send_current_prices_report.py
@@ -5,8 +5,11 @@ from reports.tasks import send_current_prices_task
 class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('--user_ids', nargs='+', type=int)
-        parser.add_argument('--category_id', type=int)
+        parser.add_argument('--category_ids', nargs='+', type=int)
 
     def handle(self, *args, **options):
         user_ids = options['user_ids']
-        send_current_prices_task(user_ids, 'category={}'.format(options['category_id']))
+
+        for category_id in options['category_ids']:
+            send_current_prices_task(user_ids, 'category={}'.format(
+                category_id))

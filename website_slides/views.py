db96427a070fad7dd128b08de422d4117785e172
Vijay Khemlani <vj@solotodo.com>
2022-12-26 17:46:19 -0300

    * Added ordering to WebsiteSlideViewset

--- website_slides/views.py
+++ website_slides/views.py
@@ -1,5 +1,6 @@
 from django_filters import rest_framework
 from rest_framework import viewsets
+from rest_framework.filters import OrderingFilter
 
 from website_slides.filters import WebsiteSlideFilterSet
 from website_slides.models import WebsiteSlide
@@ -9,6 +10,6 @@ from website_slides.serializers import WebsiteSlideSerializer
 class WebsiteSlideViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = WebsiteSlide.objects.all()
     serializer_class = WebsiteSlideSerializer
-    filter_backends = (rest_framework.DjangoFilterBackend, )
+    filter_backends = (rest_framework.DjangoFilterBackend, OrderingFilter)
     filter_class = WebsiteSlideFilterSet
 

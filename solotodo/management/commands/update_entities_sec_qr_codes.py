eff800577139311a670ebab9990391cfc3a8736b
Vijay Khemlani <vj@solotodo.com>
2023-11-06 11:06:29 -0300

    * Updated sec_qr_codes management command

--- solotodo/management/commands/update_entities_sec_qr_codes.py
+++ solotodo/management/commands/update_entities_sec_qr_codes.py
@@ -1,4 +1,5 @@
 from django.core.management import BaseCommand
+from django.db.models import Q
 
 from solotodo.models import Entity
 from solotodo.tasks import update_entity_sec_qr_codes
@@ -6,6 +7,7 @@ from solotodo.tasks import update_entity_sec_qr_codes
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        es = Entity.objects.get_active().filter(sec_qr_codes__isnull=True)
+        es = Entity.objects.get_active().filter(
+            Q(sec_qr_codes__isnull=True) | Q(sec_qr_codes='0'))
         for e in es:
             update_entity_sec_qr_codes.delay(e.id)

db3a2780542c7bb76eba693c11dc319d0663d1b5
Vijay Khemlani <vj@solotodo.com>
2024-01-24 14:19:31 -0300

    * Fixed WTB Entity available_alternatives endpoint

--- wtb/views.py
+++ wtb/views.py
@@ -12,13 +12,22 @@ from solotodo.drf_extensions import PermissionReadOnlyModelViewSet
 from solotodo.forms.category_form import CategoryForm
 from solotodo.models import Entity
 from solotodo.serializers import ProductAvailableEntitiesSerializer
-from wtb.filters import create_wtb_brand_filter, WtbEntityFilterSet, \
-    WtbBrandUpdateLogFilterSet, WtbEntityStaffFilterSet, WtbStoreFilterSet
+from wtb.filters import (
+    create_wtb_brand_filter,
+    WtbEntityFilterSet,
+    WtbBrandUpdateLogFilterSet,
+    WtbEntityStaffFilterSet,
+    WtbStoreFilterSet,
+)
 from wtb.forms import WtbEntityAssociationForm
 from wtb.models import WtbBrand, WtbEntity, WtbBrandUpdateLog
 from wtb.pagination import WtbEntityPagination, WtbStoreUpdateLogPagination
-from wtb.serializers import WtbBrandSerializer, WtbEntitySerializer, \
-    WtbBrandUpdateLogSerializer, WtbEntityStaffInfoSerializer
+from wtb.serializers import (
+    WtbBrandSerializer,
+    WtbEntitySerializer,
+    WtbBrandUpdateLogSerializer,
+    WtbEntityStaffInfoSerializer,
+)
 
 
 class WtbBrandViewSet(PermissionReadOnlyModelViewSet):
@@ -31,33 +40,35 @@ class WtbBrandViewSet(PermissionReadOnlyModelViewSet):
 
 class WtbEntityViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = WtbEntity.objects.select_related(
-        'product__instance_model',
-        'category'
-    ).prefetch_related('brand__stores')
+        "product__instance_model", "category"
+    ).prefetch_related("brand__stores")
     serializer_class = WtbEntitySerializer
     pagination_class = WtbEntityPagination
-    filter_backends = (rest_framework.DjangoFilterBackend, SearchFilter,
-                       OrderingFilter)
+    filter_backends = (rest_framework.DjangoFilterBackend, SearchFilter, OrderingFilter)
     filterset_class = WtbEntityFilterSet
-    ordering_fields = ('name', 'brand', 'last_updated', 'key', 'category',
-                       'product')
-    search_fields = ('product__instance_model__unicode_representation',
-                     'name', 'model_name', 'key', 'url')
+    ordering_fields = ("name", "brand", "last_updated", "key", "category", "product")
+    search_fields = (
+        "product__instance_model__unicode_representation",
+        "name",
+        "model_name",
+        "key",
+        "url",
+    )
 
     @action(detail=False)
     def pending(self, request):
         filterset = WtbEntityStaffFilterSet(
             queryset=self.filter_queryset(self.get_queryset()),
             data=request.query_params,
-            request=request)
+            request=request,
+        )
 
         qs = filterset.qs.get_pending()
 
         paginator = self.paginator
         page = paginator.paginate_queryset(qs, request)
 
-        serializer = WtbEntitySerializer(page, many=True,
-                                         context={'request': request})
+        serializer = WtbEntitySerializer(page, many=True, context={"request": request})
 
         return paginator.get_paginated_response(serializer.data)
 
@@ -66,25 +77,29 @@ class WtbEntityViewSet(viewsets.ReadOnlyModelViewSet):
         wtb_entity = self.get_object()
 
         if not wtb_entity.product:
-            return Response({'detail': 'Must be called on a WTB entity '
-                                       'associated with a product'},
-                            status=400)
+            return Response(
+                {
+                    "detail": "Must be called on a WTB entity "
+                    "associated with a product"
+                },
+                status=400,
+            )
 
-        field = request.GET.get('bucket_field', None)
+        field = request.GET.get("bucket_field", None)
 
         if field:
             products = wtb_entity.product.bucket([field])
         else:
             products = [wtb_entity.product]
 
-        es = Entity.objects.get_available().filter(
-            product__in=products,
-            seller__isnull=True,
-            store__in=wtb_entity.brand.stores.all()
-        ).select_related(
-            'active_registry',
-            'product__instance_model',
-            'best_coupon'
+        es = (
+            Entity.objects.get_available()
+            .filter(
+                product__in=products,
+                seller__isnull=True,
+                store__in=wtb_entity.brand.stores.all(),
+            )
+            .select_related("active_registry", "product__instance_model", "best_coupon")
         )
 
         result_dict = {}
@@ -95,11 +110,14 @@ class WtbEntityViewSet(viewsets.ReadOnlyModelViewSet):
         for entity in es:
             result_dict[entity.product].append(entity)
 
-        result_array = [{'product': product, 'entities': entities}
-                        for product, entities in result_dict.items()]
+        result_array = [
+            {"product": product, "entities": entities}
+            for product, entities in result_dict.items()
+        ]
 
         serializer = ProductAvailableEntitiesSerializer(
-            result_array, many=True, context={'request': request})
+            result_array, many=True, context={"request": request}
+        )
 
         return Response(serializer.data)
 
@@ -109,10 +127,11 @@ class WtbEntityViewSet(viewsets.ReadOnlyModelViewSet):
         if not wtb_entity.user_has_staff_perms(request.user):
             raise PermissionDenied
         serialializer = WtbEntityStaffInfoSerializer(
-            wtb_entity, context={'request': request})
+            wtb_entity, context={"request": request}
+        )
         return Response(serialializer.data)
 
-    @action(methods=['post'], detail=True)
+    @action(methods=["post"], detail=True)
     def toggle_visibility(self, request, *args, **kwargs):
         wtb_entity = self.get_object()
         if not wtb_entity.user_has_staff_perms(request.user):
@@ -122,42 +141,47 @@ class WtbEntityViewSet(viewsets.ReadOnlyModelViewSet):
             wtb_entity.is_visible = not wtb_entity.is_visible
             wtb_entity.save()
             serialized_data = WtbEntitySerializer(
-                wtb_entity, context={'request': self.request}).data
+                wtb_entity, context={"request": self.request}
+            ).data
             return Response(serialized_data)
         except IntegrityError as err:
-            return Response({'detail': str(err)},
-                            status=status.HTTP_400_BAD_REQUEST)
+            return Response({"detail": str(err)}, status=status.HTTP_400_BAD_REQUEST)
 
-    @action(methods=['post'], detail=True)
+    @action(methods=["post"], detail=True)
     def change_category(self, request, *args, **kwargs):
         wtb_entity = self.get_object()
         if not wtb_entity.user_has_staff_perms(request.user):
             raise PermissionDenied
 
         if wtb_entity.product:
-            return Response({'detail': 'Cannot change category of '
-                                       'associated entities'},
-                            status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"detail": "Cannot change category of " "associated entities"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
         form = CategoryForm(request.data)
 
         if form.is_valid():
-            new_category = form.cleaned_data['category']
+            new_category = form.cleaned_data["category"]
             if new_category == wtb_entity.category:
-                return Response({'detail': 'The new category must be '
-                                           'different from the original one'},
-                                status=status.HTTP_400_BAD_REQUEST)
+                return Response(
+                    {
+                        "detail": "The new category must be "
+                        "different from the original one"
+                    },
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
 
             wtb_entity.category = new_category
             wtb_entity.save()
             serialized_data = WtbEntitySerializer(
-                wtb_entity, context={'request': self.request}).data
+                wtb_entity, context={"request": self.request}
+            ).data
             return Response(serialized_data)
         else:
-            return Response({'detail': form.errors},
-                            status=status.HTTP_400_BAD_REQUEST)
+            return Response({"detail": form.errors}, status=status.HTTP_400_BAD_REQUEST)
 
-    @action(methods=['post'], detail=True)
+    @action(methods=["post"], detail=True)
     def associate(self, request, *args, **kwargs):
         wtb_entity = self.get_object()
         if not wtb_entity.user_has_staff_perms(request.user):
@@ -165,24 +189,21 @@ class WtbEntityViewSet(viewsets.ReadOnlyModelViewSet):
 
         form = WtbEntityAssociationForm(request.data)
         if not form.is_valid():
-            return Response({
-                'errors': form.errors
-            }, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"errors": form.errors}, status=status.HTTP_400_BAD_REQUEST)
 
-        product = form.cleaned_data['product']
+        product = form.cleaned_data["product"]
 
         try:
             wtb_entity.associate(request.user, product)
         except Exception as ex:
-            return Response(
-                {'detail': str(ex)},
-                status=status.HTTP_400_BAD_REQUEST)
+            return Response({"detail": str(ex)}, status=status.HTTP_400_BAD_REQUEST)
 
         serialized_data = WtbEntitySerializer(
-            wtb_entity, context={'request': self.request}).data
+            wtb_entity, context={"request": self.request}
+        ).data
         return Response(serialized_data)
 
-    @action(methods=['post'], detail=True)
+    @action(methods=["post"], detail=True)
     def dissociate(self, request, *args, **kwargs):
         wtb_entity = self.get_object()
         if not wtb_entity.user_has_staff_perms(request.user):
@@ -191,12 +212,11 @@ class WtbEntityViewSet(viewsets.ReadOnlyModelViewSet):
         try:
             wtb_entity.dissociate()
         except Exception as ex:
-            return Response(
-                {'detail': str(ex)},
-                status=status.HTTP_400_BAD_REQUEST)
+            return Response({"detail": str(ex)}, status=status.HTTP_400_BAD_REQUEST)
 
         serialized_data = WtbEntitySerializer(
-            wtb_entity, context={'request': self.request}).data
+            wtb_entity, context={"request": self.request}
+        ).data
         return Response(serialized_data)
 
     @action(detail=True)
@@ -204,15 +224,13 @@ class WtbEntityViewSet(viewsets.ReadOnlyModelViewSet):
         wtb_entity = self.get_object()
 
         if not wtb_entity.product_id:
-            return Response(
-                {'error': 'Please select an associated WTB Entity'})
+            return Response({"error": "Please select an associated WTB Entity"})
 
         wtb_brand = wtb_entity.brand
 
         filterset = WtbStoreFilterSet(
-            queryset=wtb_brand.stores.all(),
-            data=request.query_params,
-            request=request)
+            queryset=wtb_brand.stores.all(), data=request.query_params, request=request
+        )
 
         stores = filterset.qs
         product = wtb_entity.product
@@ -223,27 +241,26 @@ class WtbEntityViewSet(viewsets.ReadOnlyModelViewSet):
         )
 
         similar_products = product.find_similar(
-            stores=stores,
-            initial_candidate_entities=entities
-        )['similar']
+            stores=stores, initial_candidate_entities=entities
+        )["similar"]
 
-        similar_products = [e['product'] for e in similar_products]
+        similar_products = [e["product"] for e in similar_products]
 
         wtb_entities = WtbEntity.objects.filter(
-            brand=wtb_brand,
-            product__in=similar_products
-        ).select_related('product')
+            brand=wtb_brand, product__in=similar_products, is_active=True
+        ).select_related("product")
 
         product_wtb_entity_dict = {}
         for wtb_entity in wtb_entities:
             product_wtb_entity_dict[wtb_entity.product] = wtb_entity
 
-        similar_wtb_entities = [product_wtb_entity_dict[product]
-                                for product in similar_products]
+        similar_wtb_entities = [
+            product_wtb_entity_dict[product] for product in similar_products
+        ]
 
         serialized_data = WtbEntitySerializer(
-            similar_wtb_entities,
-            many=True, context={'request': self.request}).data
+            similar_wtb_entities, many=True, context={"request": self.request}
+        ).data
         return Response(serialized_data)
 
 
@@ -253,4 +270,4 @@ class WtbBrandUpdateLogViewSet(viewsets.ReadOnlyModelViewSet):
     pagination_class = WtbStoreUpdateLogPagination
     filter_backends = (rest_framework.DjangoFilterBackend, OrderingFilter)
     filterset_class = WtbBrandUpdateLogFilterSet
-    ordering_fields = ('last_updated', )
+    ordering_fields = ("last_updated",)

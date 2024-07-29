5064d71377a6cf86aa66c25547f6efd7cc94854e
Vijay Khemlani <vj@solotodo.com>
2024-07-29 16:10:58 -0400

    * Reverted removed reports

--- reports/tasks.py
+++ reports/tasks.py
@@ -7,7 +7,11 @@ from django.http import QueryDict
 
 from reports.forms.report_daily_prices_form import ReportDailyPricesForm
 from reports.forms.report_current_prices_form import ReportCurrentPricesForm
+from reports.forms.report_mercadolibre_chile_catalog_form import (
+    ReportMercadoLibreChileCatalogForm,
+)
 from reports.forms.report_store_analysis_form import ReportStoreAnalysisForm
+from reports.forms.report_store_analytics_form import ReportStoreAnalyticsForm
 from reports.forms.report_weekly_prices_form import ReportWeeklyPricesForm
 from reports.models import Report, ReportDownload
 from solotodo.models import SoloTodoUser, EsProduct
@@ -173,6 +177,82 @@ def send_weekly_prices_task(user_id, query_string):
     )
 
     email = EmailMessage("Reporte precios semanales", message, sender, [user.email])
+    email.attach(
+        report_filename,
+        report_file,
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    )
+    email.send()
+
+
+@shared_task(queue="reports", ignore_result=True, task_time_limit=1800)
+def send_report_mercadolibre_chile_catalog_task(user_id, query_string):
+    report = Report.objects.get(slug="mercadolibre_chile_catalog")
+    user = SoloTodoUser.objects.get(id=user_id)
+
+    q_dict = QueryDict(query_string)
+
+    form = ReportMercadoLibreChileCatalogForm(q_dict)
+    assert form.is_valid(), form.errors
+
+    report_data = form.generate_report()
+
+    report_filename = "mercadolibre_chile_catalog.xlsx"
+    report_file = report_data["file"]
+    report_path = report_data["path"]
+
+    ReportDownload.objects.create(report=report, user=user, file=report_path)
+
+    sender = SoloTodoUser().get_bot().email_recipient_text()
+    message = (
+        "Se adjunta el reporte de catálogo de MercadoLibre para el seller "
+        '"{}"'.format(
+            form.cleaned_data["seller_id"],
+        )
+    )
+
+    email = EmailMessage(
+        "Reporte de catálogo de MercadoLibre", message, sender, [user.email]
+    )
+    email.attach(
+        report_filename,
+        report_file,
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    )
+    email.send()
+
+
+@shared_task(queue="reports", ignore_result=True, task_time_limit=1800)
+def send_store_analytics_task(user_id, query_string):
+    report = Report.objects.get(slug="store_analytics")
+    user = SoloTodoUser.objects.get(id=user_id)
+
+    q_dict = QueryDict(query_string)
+
+    form = ReportStoreAnalyticsForm(user, q_dict)
+    assert form.is_valid(), form.errors
+
+    report_data = form.generate_report()
+
+    report_filename = "{}.xlsx".format(report_data["filename"])
+    report_file = report_data["file"]
+    report_path = report_data["path"]
+
+    ReportDownload.objects.create(report=report, user=user, file=report_path)
+
+    sender = SoloTodoUser().get_bot().email_recipient_text()
+    message = (
+        "Se adjunta el reporte de análisis de tráfico para la tienda "
+        '"{}", con fechas entre {} y {}'.format(
+            form.cleaned_data["store"],
+            form.cleaned_data["timestamp"].start.strftime("%Y-%m-%d"),
+            form.cleaned_data["timestamp"].stop.strftime("%Y-%m-%d"),
+        )
+    )
+
+    email = EmailMessage(
+        "Reporte de análisis de tráfico", message, sender, [user.email]
+    )
     email.attach(
         report_filename,
         report_file,
